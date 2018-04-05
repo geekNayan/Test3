fe0868340042e646ddabff5f91fa1ea8f7af9ce6
Nayan Kurude <geeknayan@gmail.com>
2018-04-05 12:25:51 +0530

    Initial Checkin

---
+++ TestLib4/src/main/java/com/simbalarry/testlib4/CalcMul.java
@@ -0,0 +1,9 @@
+package com.simbalarry.testlib4;
+
+public class CalcMul
+{
+    int mul(int x, int y)
+    {
+        return x*y;
+    }
+}
---
+++ testlib3/src/main/kotlin/com/simbalarry/testlib3/CalcSub.kt
@@ -0,0 +1,12 @@
+package com.simbalarry.testlib3
+
+/**
+ * Created by compilerNayan on 4/5/2018.
+ */
+class CalcSub
+{
+    fun sub(a: Int, b: Int): Int
+    {
+        return a-b;
+    }
+}
